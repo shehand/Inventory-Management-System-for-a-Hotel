efcabb183df1513d52442604e995b9192e198ee8
Shehan Dhaleesha <sonodewcomplex@gmail.com>
2018-02-12 19:33:04 +0530

    Add files via upload

---
+++ config/email.php
@@ -0,0 +1,9 @@
+<?php if(! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'crystallankaresort@gmail.com';
+$config['smtp_pass'] = 'crystal@123';
+?>
